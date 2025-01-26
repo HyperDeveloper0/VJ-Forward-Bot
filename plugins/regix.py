576f053d0ed6f29ee3f7119065d6e62cc89205ed
Hyper Developer <bishalsarkar7959@gmail.com>
2025-01-26 17:24:53 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -80,7 +80,7 @@ async def pub_(bot, message):
       await client.start()
     except Exception as e:  
       return await m.edit(e)
-    await msg_edit(m, "<code>processing..</code>")
+    await msg_edit(m, "<code>𝙋𝙧𝙤𝙘𝙚𝙨𝙨𝙞𝙣𝙜..</code>")
     try: 
        await client.get_messages(sts.get("FROM"), sts.get("limit"))
     except:
@@ -265,9 +265,9 @@ async def edit(user, msg, title, status, sts):
    estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
    estimated_total_time = estimated_total_time if estimated_total_time != '' else '0 s'
    if status in ["cancelled", "completed"]:
-      button.append([InlineKeyboardButton('• ᴄᴏᴍᴘʟᴇᴛᴇᴅ ​•', url='https://t.me/VJ_BOTZ')])
+      button.append([InlineKeyboardButton('• ᴄᴏᴍᴘʟᴇᴛᴇᴅ ​•', url='https://t.me/Reconic_Bots')])
    else:
-      button.append([InlineKeyboardButton('• ᴄᴀɴᴄᴇʟ', 'terminate_frwd')])
+      button.append([InlineKeyboardButton('• ᴄᴀɴᴄᴇʟ •', 'terminate_frwd')])
    await msg_edit(msg, text, InlineKeyboardMarkup(button))
 
 # Don't Remove Credit Tg - @VJ_Botz
