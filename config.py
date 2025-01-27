8192a0fcc0c0b1509b60cfc8dc41250b4de921f0
Hyper Developer <bishalsarkar7959@gmail.com>
2025-01-27 12:59:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,3 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
-
-API = environ.get("API", "8551e30d52309a18d0c02a1162d8baa506ca9e5d") # shortlink api
-URL = environ.get("URL", "instantearn.in") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Reconic_Bots") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "RapidForwarderBot") # bot username without @
-VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital
